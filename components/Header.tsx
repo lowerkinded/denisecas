0fe3ca5ea41a0f06963e52a014bd1135132b00ee
Maxim Sinitsin <max.sin@gmail.com>
2024-11-14 15:50:18 +0100

    Header and footer

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,48 +3,50 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <Container size={800}>
-      <Group justify="space-between" mt="xl">
-        <Text
-          size="3rem"
-          fw="bold"
-          variant="gradient"
-          gradient={{
-            from: "teal.9",
-            to: "teal.2",
-            deg: 45,
-          }}
-          component={Link}
-          href="/"
-        >
-          DENISE CAS
-        </Text>
-        <Button variant="filled" component={Link} href="/new">
-          Upload yours
-        </Button>
-      </Group>
-      <Group mt="xs" ml="2px" gap="xl">
-        <Text
-          c="teal.6"
-          td="underline"
-          component={Link}
-          href="https://denise.espritscholen.nl"
-          rel="noopener"
-          target="_blank"
-        >
-          What's DENISE?
-        </Text>
-        <Text
-          c="teal.6"
-          td="underline"
-          component={Link}
-          href="/experience/1"
-          rel="noopener"
-          target="_blank"
-        >
-          Who made this?
-        </Text>
-      </Group>
-    </Container>
+    <header>
+      <Container size={800}>
+        <Group justify="space-between" mt="xl">
+          <Text
+            size="3rem"
+            fw="bold"
+            variant="gradient"
+            gradient={{
+              from: "teal.9",
+              to: "teal.2",
+              deg: 45,
+            }}
+            component={Link}
+            href="/"
+          >
+            DENISE CAS
+          </Text>
+          <Button variant="filled" component={Link} href="/new">
+            Upload yours
+          </Button>
+        </Group>
+        <Group mt="xs" ml="2px" gap="xl">
+          <Text
+            c="teal.6"
+            td="underline"
+            component={Link}
+            href="https://denise.espritscholen.nl"
+            rel="noopener"
+            target="_blank"
+          >
+            What's DENISE?
+          </Text>
+          <Text
+            c="teal.6"
+            td="underline"
+            component={Link}
+            href="/experience/1"
+            rel="noopener"
+            target="_blank"
+          >
+            Who made this?
+          </Text>
+        </Group>
+      </Container>
+    </header>
   );
 }
