a8c7bdcf1596f60712cc6b02dfffa519f59ce3c3
Ilya Maximov <lowerkinded@gmail.com>
2025-01-03 16:23:37 +0100

    Carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,47 @@
 import { Container, Divider, Group, SimpleGrid, Text } from "@mantine/core";
+import { Carousel, CarouselSlide } from "@mantine/carousel";
 import Header from "../components/Header";
 import ExperienceCard from "../components/ExperienceCard";
+import ExperienceSlide from "../components/ExperienceSlide";
 
 export default function HomePage() {
   return (
     <>
       <Header />
+      <Carousel withIndicators loop slideSize={800} mt="xl" slideGap="md">
+        <CarouselSlide>
+          <ExperienceSlide
+            id={123}
+            type="service"
+            title="Walking my friend's dog!"
+            coverUrl="https://www.wallpaperhub.app/_next/image?url=https%3A%2F%2Fcdn.wallpaperhub.app%2Fcloudcache%2Fe%2F6%2F7%2F3%2F1%2F4%2Fe6731493cd50103e3561288c33a6a589c9bf67ab.jpg&w=384&q=75"
+          />
+        </CarouselSlide>
+        <CarouselSlide>
+          <ExperienceSlide
+            id={123}
+            type="service"
+            title="Walking my friend's dog!"
+            coverUrl="https://www.wallpaperhub.app/_next/image?url=https%3A%2F%2Fcdn.wallpaperhub.app%2Fcloudcache%2Fe%2F6%2F7%2F3%2F1%2F4%2Fe6731493cd50103e3561288c33a6a589c9bf67ab.jpg&w=384&q=75"
+          />
+        </CarouselSlide>
+        <CarouselSlide>
+          <ExperienceSlide
+            id={123}
+            type="service"
+            title="Walking my friend's dog!"
+            coverUrl="https://www.wallpaperhub.app/_next/image?url=https%3A%2F%2Fcdn.wallpaperhub.app%2Fcloudcache%2Fe%2F6%2F7%2F3%2F1%2F4%2Fe6731493cd50103e3561288c33a6a589c9bf67ab.jpg&w=384&q=75"
+          />
+        </CarouselSlide>
+        <CarouselSlide>
+          <ExperienceSlide
+            id={123}
+            type="service"
+            title="Walking my friend's dog!"
+            coverUrl="https://www.wallpaperhub.app/_next/image?url=https%3A%2F%2Fcdn.wallpaperhub.app%2Fcloudcache%2Fe%2F6%2F7%2F3%2F1%2F4%2Fe6731493cd50103e3561288c33a6a589c9bf67ab.jpg&w=384&q=75"
+          />
+        </CarouselSlide>
+      </Carousel>
       <Container size={700} mt="xl">
         <Group gap="sm">
           <Text size="1rem" fw="bold" tt="uppercase" c="gray.6">
--- components/TypeBadge.tsx
+++ components/TypeBadge.tsx
@@ -1,34 +1,54 @@
-import { Badge } from "@mantine/core";
+import { Badge, MantineSize } from "@mantine/core";
 
 export default function TypeBadge({
   type,
+  style,
+  size,
 }: {
   type: "creativity" | "activity" | "service";
+  style: "adapt" | "outline";
+  size: MantineSize;
 }) {
   const color =
     type === "creativity" ? "blue" : type === "activity" ? "red" : "teal";
 
-  return (
-    <>
-      <Badge
-        c={`${color}.5`}
-        bg={`${color}.1`}
-        radius="md"
-        size="lg"
-        darkHidden
-      >
-        {type}
-      </Badge>
+  if (style === "adapt") {
+    return (
+      <>
+        <Badge
+          c={`${color}.5`}
+          bg={`${color}.1`}
+          radius="md"
+          size={size}
+          darkHidden
+        >
+          {type}
+        </Badge>
+        <Badge
+          c={`${color}.5`}
+          bg="transparent"
+          bd={`1px solid ${color}.5`}
+          radius="md"
+          size={size}
+          lightHidden
+        >
+          {type}
+        </Badge>
+      </>
+    );
+  }
+
+  if (style === "outline") {
+    return (
       <Badge
         c={`${color}.5`}
         bg="transparent"
-        bd={`1px solid ${color}.5`}
+        bd={`2px solid ${color}.5`}
         radius="md"
-        size="lg"
-        lightHidden
+        size={size}
       >
         {type}
       </Badge>
-    </>
-  );
+    );
+  }
 }
