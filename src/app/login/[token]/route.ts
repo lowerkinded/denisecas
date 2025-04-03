e906786b695ae34b34078f339a17e1ab71f30b1a
Ilya Maximov <lowerkinded@gmail.com>
2025-04-03 19:31:13 +0200

    Prettierjs

--- src/app/login/[token]/route.ts
+++ src/app/login/[token]/route.ts
@@ -3,7 +3,7 @@ import { redirect } from "next/navigation";
 
 export async function GET(
   _request: Request,
-  { params }: { params: Promise<{ token: string }> },
+  { params }: { params: Promise<{ token: string }> }
 ) {
   const token = (await params).token;
   const cookieStore = await cookies();
--- src/lib/experience.ts
+++ src/lib/experience.ts
@@ -2,70 +2,68 @@
 
 import { neon } from "@neondatabase/serverless";
 
-export async function getExperience(
-    id: string,
-): Promise<
-    {
-        id: number;
-        author_email: string;
-        author_name: string;
-        author_picture_url: string;
-        type: "creativity" | "activity" | "service";
-        from_date: Date;
-        to_date: Date;
-        summary: string;
-        cover_url: string | null;
-        title: string;
-        md_description: string;
-        front_page_position: number | null;
-        carousel_position: number | null;
-    } | undefined
+export async function getExperience(id: string): Promise<
+  | {
+      id: number;
+      author_email: string;
+      author_name: string;
+      author_picture_url: string;
+      type: "creativity" | "activity" | "service";
+      from_date: Date;
+      to_date: Date;
+      summary: string;
+      cover_url: string | null;
+      title: string;
+      md_description: string;
+      front_page_position: number | null;
+      carousel_position: number | null;
+    }
+  | undefined
 > {
-    const sql = neon(`${process.env.DATABASE_URL}`);
-    const response = await sql`SELECT * FROM experience WHERE id = ${id}`;
+  const sql = neon(`${process.env.DATABASE_URL}`);
+  const response = await sql`SELECT * FROM experience WHERE id = ${id}`;
 
-    return response[0] as any;
+  return response[0] as any;
 }
 
 export async function getExperiences(): Promise<
-    {
-        id: number;
-        author_email: string;
-        author_name: string;
-        author_picture_url: string;
-        type: "creativity" | "activity" | "service";
-        from_date: Date;
-        to_date: Date;
-        summary: string;
-        cover_url: string | null;
-        title: string;
-        md_description: string;
-        front_page_position: number | null;
-        carousel_position: number | null;
-    }[]
-> {
-    const sql = neon(`${process.env.DATABASE_URL}`);
-    const response =
-        await sql`SELECT * FROM experience ORDER BY author_name ASC`;
-
-    return response as any;
-}
-
-export async function insertExperience(experience: {
+  {
+    id: number;
     author_email: string;
     author_name: string;
-    // author_picture_url: string;
+    author_picture_url: string;
     type: "creativity" | "activity" | "service";
     from_date: Date;
     to_date: Date;
     summary: string;
-    // cover_url: string | null;
+    cover_url: string | null;
     title: string;
     md_description: string;
-}): Promise<{ id: number }> {
-    const sql = neon(`${process.env.DATABASE_URL}`);
+    front_page_position: number | null;
+    carousel_position: number | null;
+  }[]
+> {
+  const sql = neon(`${process.env.DATABASE_URL}`);
+  const response = await sql`SELECT * FROM experience ORDER BY author_name ASC`;
 
-    const response = await sql`
+  return response as any;
+}
+
+export async function insertExperience(experience: {
+  author_email: string;
+  author_name: string;
+  // author_picture_url: string;
+  type: "creativity" | "activity" | "service";
+  from_date: Date;
+  to_date: Date;
+  summary: string;
+  // cover_url: string | null;
+  title: string;
+  md_description: string;
+}): Promise<{ id: number }> {
+  const sql = neon(`${process.env.DATABASE_URL}`);
+
+  const response = await sql`
     INSERT INTO experience (
         author_email,
         author_name,
@@ -86,10 +84,12 @@ export async function insertExperience(experience: {
         ${experience.md_description}
     ) RETURNING id`;
 
-    return response[0] as any;
+  return response[0] as any;
 }
 
-export async function updateExperience(id: number, experience: {
+export async function updateExperience(
+  id: number,
+  experience: {
     author_email: string;
     author_name: string;
     // author_picture_url: string;
@@ -100,10 +100,11 @@ export async function updateExperience(id: number, experience: {
     // cover_url: string | null;
     title: string;
     md_description: string;
-}): Promise<void> {
-    const sql = neon(`${process.env.DATABASE_URL}`);
+  }
+): Promise<void> {
+  const sql = neon(`${process.env.DATABASE_URL}`);
 
-    await sql`
+  await sql`
     UPDATE experience 
     SET author_email = ${experience.author_email},
         author_name = ${experience.author_name},
@@ -117,71 +118,73 @@ export async function updateExperience(id: number, experience: {
 }
 
 export async function deleteExperience(id: string): Promise<void> {
-    const sql = neon(`${process.env.DATABASE_URL}`);
+  const sql = neon(`${process.env.DATABASE_URL}`);
 
-    await sql`DELETE FROM experience WHERE id = ${id}`;
+  await sql`DELETE FROM experience WHERE id = ${id}`;
 }
 
 export async function setCarouselPosition(
-    id: number | string,
-    position: number | null,
+  id: number | string,
+  position: number | null
 ): Promise<void> {
-    const sql = neon(`${process.env.DATABASE_URL}`);
+  const sql = neon(`${process.env.DATABASE_URL}`);
 
-    await sql`UPDATE experience SET carousel_position = ${position} WHERE id = ${id}`;
+  await sql`UPDATE experience SET carousel_position = ${position} WHERE id = ${id}`;
 }
 
 export async function setCarouselPositions(
-    values: { id: number; position: number | null }[],
+  values: { id: number; position: number | null }[]
 ) {
-    const sql = neon(`${process.env.DATABASE_URL}`);
+  const sql = neon(`${process.env.DATABASE_URL}`);
 
-    const valuesSql = values
-        .map((it, i) =>
-            `($${i * 2 + 1}::integer, $${i * 2 + 2}${
-                it === null ? "" : "::integer"
-            })`
-        )
-        .join(",");
+  const valuesSql = values
+    .map(
+      (it, i) =>
+        `($${i * 2 + 1}::integer, $${i * 2 + 2}${
+          it === null ? "" : "::integer"
+        })`
+    )
+    .join(",");
 
-    await sql(
-        `
+  await sql(
+    `
         UPDATE experience
         SET carousel_position = data.position
         FROM (VALUES ${valuesSql}) AS data(id, position)
         WHERE experience.id = data.id`,
-        values.flatMap((it) => [it.id, it.position]),
-    );
+    values.flatMap((it) => [it.id, it.position])
+  );
 }
 
 export async function setFrontPagePosition(
-    id: number | string,
-    position: number | null,
+  id: number | string,
+  position: number | null
 ): Promise<void> {
-    const sql = neon(`${process.env.DATABASE_URL}`);
+  const sql = neon(`${process.env.DATABASE_URL}`);
 
-    await sql`UPDATE experience SET front_page_position = ${position} WHERE id = ${id}`;
+  await sql`UPDATE experience SET front_page_position = ${position} WHERE id = ${id}`;
 }
 
 export async function setFrontPagePositions(
-    values: { id: number; position: number | null }[],
+  values: { id: number; position: number | null }[]
 ) {
-    const sql = neon(`${process.env.DATABASE_URL}`);
+  const sql = neon(`${process.env.DATABASE_URL}`);
 
-    const valuesSql = values
-        .map((it, i) =>
-            `($${i * 2 + 1}::integer, $${i * 2 + 2}${
-                it === null ? "" : "::integer"
-            })`
-        )
-        .join(",");
+  const valuesSql = values
+    .map(
+      (it, i) =>
+        `($${i * 2 + 1}::integer, $${i * 2 + 2}${
+          it === null ? "" : "::integer"
+        })`
+    )
+    .join(",");
 
-    await sql(
-        `
+  await sql(
+    `
         UPDATE experience
         SET front_page_position = data.position
         FROM (VALUES ${valuesSql}) AS data(id, position)
         WHERE experience.id = data.id`,
-        values.flatMap((it) => [it.id, it.position]),
-    );
+    values.flatMap((it) => [it.id, it.position])
+  );
 }
