a0a424f396a7b214cb04c8621c5db2059232f05f
Ilya Maximov <lowerkinded@gmail.com>
2025-04-05 04:18:51 +0200

    Format nitpick

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@mantine/core/styles.css";
 import "@mantine/carousel/styles.css";
 import "@mantine/dates/styles.css";
+
 import React from "react";
 import { MantineProvider, ColorSchemeScript } from "@mantine/core";
 import { theme } from "@/theme";
